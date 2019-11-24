fbb20a9829f4e4abca06e9cebfceaccdba34ec40
Vincent LAURENS <vlaurens@etud.insa-toulouse.fr>
2019-11-24 19:17:24 +0100

    ADD Alarm triggered on oneM2M

--- Master/src/main/java/fr/project_soa/master/use_case_1/UC1_main.java
+++ Master/src/main/java/fr/project_soa/master/use_case_1/UC1_main.java
@@ -1,11 +1,12 @@
 package fr.project_soa.master.use_case_1;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+
 import javax.ws.rs.core.Response;
+import fr.project_soa.master.client.Client;
 
 import fr.project_soa.master.Config;
 
+import java.io.IOException;
 import java.time.*;
 
 public class UC1_main {
@@ -18,14 +19,42 @@ public class UC1_main {
 		this.id = id;
 	}
 
-	public String triggerAlarm(boolean presence, String status) {
+	public String triggerAlarm(boolean presence, String status) throws IOException {
 		LocalTime currentTime = LocalTime.now();
+		Client client = new Client();
+		
 		if (currentTime.getHour()>23 || currentTime.getHour()<7 && presence == true) {
 			System.out.println("Trigger Alarm");
 			//Post OM2M cin_Alarm
+			
+			String cinAlarm = "<m2m:cin xmlns:m2m=\"http://www.onem2m.org/xml/protocols\">"+ 
+				    "<cnf>message</cnf>"+
+				    "<con>"+
+				      "&lt;obj&gt;"+
+				        "&lt;str name=&quot;category&quot; val=&quot;Alarm &quot;/&gt;"+
+				        "&lt;int name=&quot;data&quot; val=&quot;ON&quot;/&gt;"+
+				      "&lt;/obj&gt;"+
+				    "</con>"+
+				"</m2m:cin>";
+			
+			fr.project_soa.master.client.Response resp = client.update("http://localhost:8080/~/in-cse/in-name/Floor1_Manager/Alarm", cinAlarm, "admin:admin");
+			System.out.println(resp.toString());
+			System.out.println("Alarm triggered");
 		}
 		else {
 			System.out.println("Alarm not triggered");
+			String cinAlarm = "<m2m:cin xmlns:m2m=\"http://www.onem2m.org/xml/protocols\">"+ 
+				    "<cnf>message</cnf>"+
+				    "<con>"+
+				      "&lt;obj&gt;"+
+				        "&lt;str name=&quot;category&quot; val=&quot;Alarm &quot;/&gt;"+
+				        "&lt;int name=&quot;data&quot; val=&quot;OFF&quot;/&gt;"+
+				      "&lt;/obj&gt;"+
+				    "</con>"+
+				"</m2m:cin>";
+			
+			fr.project_soa.master.client.Response resp = client.update("http://localhost:8080/~/in-cse/in-name/Floor1_Manager/Alarm", cinAlarm, "admin:admin");
+			System.out.println(resp.toString());
 		}
 		
 		System.out.println("It is : " + currentTime);
